8dfdb96f0b0e4bf23b4f901bbe6285c71dd43eec
Marcel <Marcel.Voigt92@gmx.de>
2023-02-01 19:07:28 +0100

    changed styling and added a handleDelete function

--- article-manager/src/pages/ArticlePage.jsx
+++ article-manager/src/pages/ArticlePage.jsx
@@ -1,14 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
+import "./styles/Article.css";
 
 export const ArticlePage = () => {
   const { id } = useParams();
   const url = `http://localhost:4000/articles/${id}`;
+  const [showModal, setShowModal] = useState(false);
 
   const { data: article, isPending: loading, error } = useFetch(url);
 
   const history = useHistory();
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:4000/articles/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        setShowModal(true);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    history.push("/");
+  };
 
   useEffect(() => {
     if (error) {
@@ -23,11 +44,20 @@ export const ArticlePage = () => {
       {error && <h2>{error}</h2>}
 
       {article && (
-        <div>
+        <div className="article-container">
           <h1>Article by {article.author}</h1>
           <h2>{article.title}</h2>
           <p>{article.body}</p>
           <img src={article.imageUrl} alt="" />
+          <button onClick={() => handleDelete(article.id)}>Delete</button>
+        </div>
+      )}
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h2>Article deleted successfully</h2>
+            <button onClick={closeModal}>Close</button>
+          </div>
         </div>
       )}
     </div>
